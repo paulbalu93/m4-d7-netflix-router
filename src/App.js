c6783cec8405a5c412f69d9e39a55d1ed44a79df
Paul Maliakel <paulbalu93@gmail.com>
2020-11-24 16:34:45 +0100

    router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import MoviesContainer from "./Components/MoviesContainer";
+import Navbar from "./Component/NavBar";
+import Jumbotron from "./Component/Jumbotron";
+import Footer from "./Component/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Navbar />
+        <Jumbotron />
+        <MoviesContainer />
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default App;
